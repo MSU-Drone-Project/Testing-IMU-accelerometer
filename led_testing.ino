d33b4c072c9851b078d44ae04491d12468d2a8c7
Bruce Liu <lightliudark@gmail.com>
2020-09-16 01:36:09 -0600

    Add files via upload

---
+++ led_testing.ino
@@ -0,0 +1,14 @@
+int top_left_pin = 13;
+void setup() {
+  pinMode(top_left_pin,OUTPUT);
+
+}
+
+void loop() {
+
+digitalWrite(top_left_pin,HIGH);
+delay(500);
+
+digitalWrite(top_left_pin,LOW);
+delay(500);
+}
